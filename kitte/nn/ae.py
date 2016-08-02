968c005276f43c8ae1fde9214cdbf4f30ca67e13
nie <5861189@qq.com>
2016-08-02 12:20:09 +0800

    *** empty log message ***

--- kitte/nn/ae.py
+++ kitte/nn/ae.py
@@ -16,50 +16,63 @@ from ..models.ucf_result import Ucf_Result
 
 log = logging.getLogger(__name__)
 
-DATA_PATH = "./model.ckpt"
+flags = tf.app.flags
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('MODEL_PATH', 'model.ckpt',
+                    'Directory to put the training data.')
 
 class Autoencoder(object):
 
     def __init__(self, feature_lens):
 
-        self.feature_lens = feature_lens
-        self.hidden_lens = feature_lens/2
-        self.learning_rate = 0.01
-        self.saver = tf.train.Saver()
+        hidden_lens = int(feature_lens/2)
+        learning_rate = 0.01
+        self.X = tf.placeholder(tf.float32, shape=[None, feature_lens])
 
-        self.X = tf.placeholder("float", [self.feature_lens])
-
-        encoder_h = tf.Variable(tf.random_normal([self.feature_lens])),
-        encoder_b = tf.Variable(tf.random_normal([self.hidden_lens])),
+        encoder_h = tf.Variable(
+            tf.random_normal([feature_lens, hidden_lens]))
         
-        self.hidden_layer = tf.nn.sigmoid(
-            tf.add(tf.matmul(self.X, encoder_h), encoder_b))
+        encoder_b = tf.Variable(
+            tf.random_normal([hidden_lens]))
+        
+        hidden_layer = tf.nn.sigmoid(
+            tf.add(
+                tf.matmul(self.X, encoder_h), encoder_b))
 
-        decoder_h = tf.transpose(encoder_h)
-        decoder_b = encoder_b
-        self.final_layer = tf.nn.sigmoid(
-                tf.add(tf.matmul(self.hidden_layer, decoder_h), decoder_b))
+        decoder_h = tf.Variable(
+            tf.random_normal([hidden_lens, feature_lens]))
+
+        decoder_b = tf.Variable(
+            tf.random_normal([feature_lens]))
+        final_layer = tf.nn.sigmoid(
+            tf.add(
+                tf.matmul(hidden_layer, decoder_h), decoder_b))
 
         # Define loss and optimizer, minimize the squared error
         self.cost = tf.reduce_mean(
-            tf.pow(self.X - self.final_layer, 2))
+            tf.pow(self.X - final_layer, 2))
         self.optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(self.cost)
         init = tf.initialize_all_variables()
         # Launch the session
         self.sess = tf.Session()
+        # self.saver.save(self.sess, MODEL_PATH)
         self.sess.run(init)
         return 
 
-    def train(self, train_data):
+    def train(self, train_data, feature_lens):
+        train_data = np.array(train_data)
         opt, cost = self.sess.run((self.optimizer, self.cost), 
                                   feed_dict={self.X: train_data})
         log.info(cost)
+        # self.saver.save(self.sess, MODEL_PATH)
         return
 
     def _del_(self):
-        saver.save(self.sess, DATA_PATH)
+        saver = tf.train.Saver()
+        saver.save(self.sess, FLAGS.MODEL_PATH, global_step=1)
         self.session.close()
-
+        return
 
 def _get_all_products():
     """
@@ -89,7 +102,7 @@ def _get_one_user_data(user_id):
     """
     DBConnecion = get_connection()
     sql_str = text("""select distinct product_id from sorder_line where user_id=:user_id""")
-    sql_answer = DBConnecion.execute(sql_str, user_id).fetchall()
+    sql_answer = DBConnecion.execute(sql_str, user_id=user_id).fetchall()
     user_products = [one_product[0] for one_product in sql_answer]
     return user_products
 
@@ -104,7 +117,7 @@ def format_data(all_products, user_products):
             fdata += [1]
         else:
             fdata += [0]
-    return
+    return fdata
 
             
 def do_ae_model():
@@ -112,20 +125,17 @@ def do_ae_model():
     all_products = _get_all_products()
     ## 构建模型
     ae = Autoencoder(len(all_products))
-    all_users = get_all_users()
-    train_data = []
-    for one_user in all_users():
-        user_products = _get_one_user_data(user_id)
+    all_users = _get_all_users()
+    for one_user_id in all_users:
+        user_products = _get_one_user_data(one_user_id)
         ## 如何用稀疏数据表示
         fdata = format_data(all_products, user_products)
-        train_data += [fdata]
-        if len(train_data)>=1000:
-            ae.train(train_data)
-            train_data = []
+        train_data = [fdata]
+        ae.train(train_data, len(all_products))
     return 
     
 
-def job():
-    do_ae_model()
-    return
+# def job():
+#     do_ae_model()
+#     return
 
